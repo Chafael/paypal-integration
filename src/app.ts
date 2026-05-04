02ac8f904f4ccde9cd07c3124732734452c9ed11
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-05-04 11:45:55 -0600

    configuracion inicial de express con un logger basico

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.url);
+  next();
+});
+export default app;
