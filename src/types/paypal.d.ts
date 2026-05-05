e06983567ed00256544b61d09fdb3a0ae00caee6
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-05-04 18:02:11 -0600

    borro el tipo de prueba que ya no voy a usar

--- src/types/paypal.d.ts
+++ src/types/paypal.d.ts
@@ -1,7 +1,2 @@
 // Los tipos del SDK de PayPal son provistos por @types/paypal__checkout-server-sdk
 // No se necesita declaración manual aquí
-
-export type TmpTestUser = {
-  id: string;
-  token: string;
-};
