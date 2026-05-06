74813e11604fc7c9c02324a90c379147a2764005
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-05-06 10:48:21 -0600

    agrego las rutas y unos todos pendientes

---
+++ src/routes/paypal.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+  createOrder,
+  captureOrder,
+  getOrder,
+} from '../controllers/paypal.controller';
+
+const router = Router();
+
+router.post('/orders', createOrder);
+router.post('/orders/:id/capture', captureOrder);
+router.get('/orders/:id', getOrder);
+
+export default router;
+// TODO: add route for testing
+// router.get('/test', (req, res) => res.send('ok'));
+// router.get('/debug', (req, res) => res.send('debug'));
+// router.get('/status', (req, res) => res.send('status'));
