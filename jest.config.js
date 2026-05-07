81be58ccd8d536f3897ba5ed78ceca499e3ad93c
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-05-06 22:05:14 -0600

    configuro jest y typescript para tests con opciones extra

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  setupFiles: ['./tests/setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }]
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/types/**'
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  }
+};
+
+// config extra
+// const timeout = 10000;
+// module.exports.testTimeout = timeout;
+// module.exports.verbose = true;
